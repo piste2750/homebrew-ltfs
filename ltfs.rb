21ebf47ce9719df89c70bbe49bb1de5f3ba37070
Atsushi Abe <piste.2750@gmail.com>
2017-10-26 07:40:16 +0900

    Initial put of ltfs formula
    This formula does not work properly on my environment. I will check this formula on other environments to analize this is an environmental problem or not.

---
+++ ltfs.rb
@@ -0,0 +1,44 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook.html
+#                http://www.rubydoc.info/github/Homebrew/brew/master/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+
+class Ltfs < Formula
+  desc "Reference implementation of the LTFS format Spec for stand alone tape drive"
+  homepage ""
+  url "https://github.com/LinearTapeFileSystem/ltfs/archive/master.tar.gz#/ltfs-2.4.0.0.tar.gz"
+  sha256 "dd190dffb33cdf99b818c0ad7a8fe8328a0c857933d328c7090401c6ddacf131"
+
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "libtool" => :build
+  depends_on "pkg-config" => :build
+  depends_on :osxfuse
+  depends_on "ossp-uuid" => :build
+  depends_on "libxml2" => :build
+  depends_on "net-snmp" => :build
+  depends_on "icu4c" => :build
+  depends_on "gnu-sed" => :build 
+
+  def install
+    ENV.deparallelize
+    #ENV['CC'] = 'gcc'
+
+    system "./autogen.sh"
+    system "./configure", "--prefix=#{prefix}"
+    system "make" 
+    system "make", "install" 
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test ltfs`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
