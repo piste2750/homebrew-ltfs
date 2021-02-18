484e01ed1b647631c9a852925f1067ac47ac8156
Atsushi Abe <piste.2750@gmail.com>
2021-02-18 15:32:07 +0900

    Update version to 2.4.3.1 (10461)

--- ltfs.rb
+++ ltfs.rb
@@ -3,36 +3,44 @@
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 
 class Ltfs < Formula
-  desc "Reference implementation of the LTFS format Spec for stand alone tape drive"
-  homepage ""
-  url "https://github.com/LinearTapeFileSystem/ltfs/archive/v2.4.2.0-10418.tar.gz"
-  sha256 "cc6ae69f56b48ab09263eed40147b039a00e0440027159b40dac2b147cdb5e57"
+  desc 'Reference implementation of the LTFS format Spec for stand alone tape drive'
+  homepage 'https://github.com/LinearTapeFileSystem/ltfs'
+  url 'https://github.com/LinearTapeFileSystem/ltfs/archive/v2.4.3.1-10461.tar.gz'
+  sha256 'ae39778a47cb09c126fd394c269cbf464f7b43c876ea71a9c880e1159ce87066'
 
   head do
-    url "https://github.com/LinearTapeFileSystem/ltfs.git"
+    url 'https://github.com/LinearTapeFileSystem/ltfs.git'
   end
 
-  depends_on "autoconf" => :build
-  depends_on "automake" => :build
-  depends_on "libtool" => :build
-  depends_on "pkg-config" => :build
-  depends_on "gnu-sed" => :build
-  depends_on :osxfuse
-  depends_on "ossp-uuid"
-  depends_on "libxml2"
-  depends_on "icu4c"
+  depends_on 'autoconf' => :build
+  depends_on 'automake' => :build
+  depends_on 'libtool' => :build
+  depends_on 'pkg-config' => :build
+  depends_on 'gnu-sed' => :build
+  depends_on 'ossp-uuid'
+  depends_on 'libxml2'
+  depends_on 'icu4c'
+
+  on_macos do
+    deprecate! date: "2021-02-18", because: "requires FUSE"
+    depends_on :osxfuse
+    ENV['LDFLAGS'] = '-framework CoreFoundation -framework IOKit'
+  end
+
+  on_linux do
+    depends_on "libfuse"
+  end
 
   env :std
 
   def install
     ENV.deparallelize
     ENV['CC'] = 'gcc'
-    ENV['LDFLAGS'] = '-framework CoreFoundation -framework IOKit'
 
-    system "./autogen.sh"
-    system "./configure", "--prefix=#{prefix}", "--disable-snmp"
-    system "make"
-    system "make", "install"
+    system './autogen.sh'
+    system './configure', "--prefix=#{prefix}", '--disable-snmp'
+    system 'make'
+    system 'make', 'install'
   end
 
   test do
@@ -44,7 +52,7 @@ class Ltfs < Formula
     # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
     #
     # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
-    system "false"
+    # executables being tested: `system '#{bin}/program', 'do', 'something'`.
+    system 'false'
   end
 end
